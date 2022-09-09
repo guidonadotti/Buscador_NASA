692b2dee758426e227f62e36c5410f18e4d8710c
Lihuén <110670239+lud-lihuen@users.noreply.github.com>
2022-09-09 09:25:38 -0300

    Detalles

--- js/space.js
+++ js/space.js
@@ -1,37 +1,36 @@
 
-let getJSONData = function (url) {
+let getJSONData = function(url) {
     let result = {};
 
     return fetch(url)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then(function (response) {
-            result.status = 'ok';
-            result.data = response;
-       
-            return result;
-        })
-        .catch(function (error) {
-            result.status = 'error';
-            result.data = error;
-    
-            return result;
-        });
-}
+    .then(function(response) {
+         if (response.ok) {
+            return response.json();
+        } else {
+            throw Error(response.statusText);
+        }
+    })
+    .then(function(response) {
+        result.status = 'ok';
+        result.data = response;
+   
+        return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
 
+        return result;
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("btnBuscar").addEventListener("click", () => {
-        document.getElementById("contenedor").innerHTML=""
-        let Busqueda = document.getElementById("inputBuscar").value
-        let JSON = `https://images-api.nasa.gov/search?q=${Busqueda}`
+        let busqueda = document.getElementById("inputBuscar").value;
+        let JSON = `https://images-api.nasa.gov/search?q=${busqueda}`;
+        document.getElementById("contenedor").innerHTML = "";
         getJSONData(JSON).then(respuesta => {
-            respuesta=respuesta.data
+            respuesta = respuesta.data;
             for (elemento of respuesta.collection.items) {
                 document.getElementById("contenedor").innerHTML += `
                 <div class="col-4">
@@ -49,4 +48,4 @@ document.addEventListener("DOMContentLoaded", () => {
         })
 
     })
-})
+});
